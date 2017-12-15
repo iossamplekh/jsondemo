d6c9cc14bc194051111666cadfa9be86dd01f97b
rithkh2 <rithronlkh@gmail.com>
2017-12-15 17:43:59 +0700

    test

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,16 +5,16 @@ import UIKit
 let url = Bundle.main.url(forResource: "test", withExtension: "json")
 let data = try! Data(contentsOf: url!)
 
-print(data.count)
+//print(data)
 
 do {
     let object = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
     if let dictionary = object as? [String:Any]{
         let title = dictionary["dataTitle"]
-        let version = dictionary["swifVersion"]
-        
+        let version = dictionary["swiftVersion"]
+
         let users = dictionary["users"] as? NSArray
-        
+
         print(title!)
         print(version!)
         for user in users!{
@@ -25,3 +25,20 @@ do {
 }catch let error{
     print(error)
 }
+
+//{
+//    "dataTitle": "JSON Tutorial",
+//    "swiftVersion": 3.0,
+//    "users": [
+//    {
+//    "name":"Dara",
+//    "age": 30
+//    },
+//    {
+//    "name":"Chan",
+//    "age": 20
+//    }
+//    ]
+//}
+
+
